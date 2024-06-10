cd957f48f0ae579b6d42b0f123fe72473860180c
PrakGov <55034062+prak112@users.noreply.github.com>
2024-06-10 16:54:26 +0300

    chore: Update GitHub repository link

--- bloglist-frontend/src/components/Footer.jsx
+++ bloglist-frontend/src/components/Footer.jsx
@@ -3,7 +3,7 @@ return (
     <footer style={{ backgroundColor: 'grey', border: '1px solid black', borderRadius: '10px', textAlign: 'center' }}>
         <div>
             <p style={{ color: 'yellow' }}>Blogs List App by Prakirth Govardhanam, June 2024</p>
-            <p><a href="https://github.com/prak112/FullStackOpen-core-part5/bloglist-frontend">GitHub Repository</a></p>
+            <p><a href="https://github.com/prak112/FullStackOpen-core-part5/">Source Code (GitHub)</a></p>
         </div>
     </footer>
 );
