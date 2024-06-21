48764e9ede008fda7152fc32c456ad74120e879c
PrakGov <55034062+prak112@users.noreply.github.com>
2024-06-21 18:24:26 +0300

    feat: Add functions for login and adding a blog

---
+++ E2E-tests/tests/helper.js
@@ -0,0 +1,15 @@
+const loginWith = async(page, username, password) => {
+    await page.getByTestId('username').fill(username)
+    await page.getByTestId('password').fill(password)
+    await page.getByTestId('login').click()
+}
+
+const addBlogTo = async(page, title, author, url) => {
+    await page.getByRole('button', { name: 'Add Blog' }).click()
+    await page.getByTestId('title').fill(title)
+    await page.getByTestId('author').fill(author)
+    await page.getByTestId('url').fill(url)
+    await page.getByTestId('add-blog').click()
+}
+
+export default { loginWith, addBlogTo }
